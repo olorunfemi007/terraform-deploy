7ae8853e5e61544f33c069e85961cb0ff40d4a5f
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2024-06-17 22:28:35 -0400

    Github Actions

--- ec2.tf
+++ ec2.tf
@@ -39,31 +39,31 @@ resource "aws_route" "public_internet_access" {
   gateway_id = aws_internet_gateway.main.id
 }
 
-resource "aws_eip" "nat_eip" {
-  vpc = true
-}
+# resource "aws_eip" "nat_eip" {
+#   vpc = true
+# }
 
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
-}
+# resource "aws_nat_gateway" "nat_gateway" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.public.id
+# }
 
-# Create a route table for the private subnet
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main.id
+# # Create a route table for the private subnet
+# resource "aws_route_table" "private_route_table" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat_gateway.id
+#   }
+# }
 
-# Associate the private route table with the private subnet
-resource "aws_route_table_association" "private_subnet_association" {
-  count = 4
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = aws_route_table.private_route_table.id
-}
+# # Associate the private route table with the private subnet
+# resource "aws_route_table_association" "private_subnet_association" {
+#   count = 4
+#   subnet_id      = element(aws_subnet.private.*.id, count.index)
+#   route_table_id = aws_route_table.private_route_table.id
+# }
 
 
 resource "aws_instance" "public_instance" {
